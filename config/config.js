0bade98b2e12740c0f3425fb8d15d53a5e8577fb
tylerphopho <tyler.phopho@gmail.com>
2020-05-06 18:35:44 -0600

    created config.js file

---
+++ config/config.js
@@ -0,0 +1,23 @@
+/* eslint-disable camelcase */
+module.exports = {
+    development: {
+      username: "root",
+      password: process.env.MYSQL_PASSWORD,
+      database: "delivery_db",
+      host: "127.0.0.1",
+      dialect: "mysql",
+      operatorsAliases: false
+    },
+    test: {
+      username: "root",
+      password: process.env.MYSQL_PASSWORD,
+      database: "delivery_db",
+      host: "127.0.0.1",
+      dialect: "mysql",
+      operatorsAliases: false
+    },
+    production: {
+      use_env_variable:"JAWSDB_URL",
+      dialect: "mysql"
+    }
+  };
